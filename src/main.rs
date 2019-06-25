f77c11ef49687f4a5a68465d8dc335665a0538fc
Denis Bazhenov <bazhenov@farpost.com>
2019-06-25 18:46:04 +1000

    -t and -s options

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Arg, App};
-use tensorflow::{Graph, Session, SessionOptions, Operation};
+use tensorflow::{Graph, Session, SessionOptions, Operation, Shape};
 use std::error::Error;
 use std::str::Utf8Error;
 use std::path::Path;
@@ -15,20 +15,32 @@ fn main() -> Result<(), Box<dyn Error>> {
 				 .takes_value(true)
 				 .required(true)
 				 .value_name("DIRECTORY"))
+		.arg_from_usage("[report_shape] -s 'Show the shape of the tensor'")
+		.arg_from_usage("[report_type] -t 'Show the type of the tensor'")
 		.get_matches();
 
 	let model_path = app.value_of("model").unwrap();
 
-	inspect(Path::new(model_path))
+	let opts = Opts {
+		report_shape: app.is_present("report_shape"),
+		report_type: app.is_present("report_type")
+	};
+
+	inspect(Path::new(model_path), &opts)
 }
 
-fn inspect(path: &Path) -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+struct Opts {
+	report_shape: bool,
+	report_type: bool
+}
+
+fn inspect(path: &Path, opts: &Opts) -> Result<(), Box<dyn Error>> {
 	let g = Graph::new();
 
 	let mut graph = Graph::new();
 	let tags: Vec<&str> = vec!["serve"];
-	let opts = SessionOptions::new();
-	let session = Session::from_saved_model(&opts, tags, &mut graph, path)?;
+	let session = Session::from_saved_model(&SessionOptions::new(), tags, &mut graph, path)?;
 
 	if let Ok(devices) = session.device_list() {
 		for ref d in devices {
@@ -38,7 +50,7 @@ fn inspect(path: &Path) -> Result<(), Box<dyn Error>> {
 	}
 
 	for o in graph.operation_iter() {
-		report_op(&o, &g);
+		report_op(&o, opts);
 	}
 
 	//let op = graph.operation_by_name_required("embedding_input")?;
@@ -46,21 +58,18 @@ fn inspect(path: &Path) -> Result<(), Box<dyn Error>> {
 	Ok(())
 }
 
-fn report_op(op: &Operation, g: &Graph) -> Result<(), Utf8Error> {
+fn report_op(op: &Operation, opts: &Opts) -> Result<(), Utf8Error> {
 	println!("{}", op.name()?);
-	// println!("   input type: {}", op.input_type(0));
-	// println!("   output type: {}", op.output_type(0));
-	// println!("   op_type: {}", op.op_type()?);
-	// println!("   num_control_inputs: {}", op.num_control_inputs());
-	// println!("   num_control_outputs: {}", op.num_control_outputs());
-	// println!("   num_inputs: {}", op.num_inputs());
-	// println!("   num_outputs: {}", op.num_outputs());
-	// if let Ok(shape) = op.get_attr_shape("shape") {
-	// 	println!("   shape: {}", shape);
-	// }
-	// if let Ok(dtype) = op.get_attr_type("dtype") {
-	// 	println!("   type: {}", dtype);
-	// }
+	if opts.report_shape {
+		if let Ok(shape) = op.get_attr_shape("shape")  {
+			println!("   shape: {}", format_shape(&shape));
+		}
+	}
+	if opts.report_type {
+		if let Ok(dtype) = op.get_attr_type("dtype") {
+			println!("   type: {}", dtype);
+		}
+	}
 
 	// if let Ok(v) = op.get_attr_tensor::<f32>("value") {
 	// 	println!("   value: {}", v);
@@ -68,6 +77,21 @@ fn report_op(op: &Operation, g: &Graph) -> Result<(), Utf8Error> {
 	// }
 	
 	Ok(())
+}
+
+fn format_shape(shape: &Shape) -> String {
+	if let Some(size) = shape.dims() {
+		let mut s = String::new();
+		for i in 0..size {
+			if i > 0 {
+				s.push_str(", ");
+			}
+			s.push_str(&shape[i].unwrap_or(-1).to_string());
+		}
+		s
+	} else {
+		"-1".to_string()
+	}
 }
 
 fn format_dimensions(dims: &[u64]) -> String {
@@ -84,5 +108,13 @@ mod tests {
 	#[test]
 	fn test_format_dimensions() {
 		assert_eq!(format_dimensions(&[1, 2, 3]), "1x2x3");
+	}
+
+	#[test]
+	fn test_format_shape() {
+		assert_eq!(format_shape(&Shape::from(Some(vec![Some(1), Some(2)]))), "1, 2");
+		assert_eq!(format_shape(&Shape::from(Some(vec![None, Some(2)]))), "-1, 2");
+		assert_eq!(format_shape(&Shape::from(None)), "-1");
+		assert_eq!(format_shape(&Shape::from(Some(vec![]))), "");
 	}
 }
